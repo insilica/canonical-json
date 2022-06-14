7de74f2fffac2b31ed2940b5ee91d6ff0e8ac219
John Shaffer <jdsha@proton.me>
2022-06-14 15:36:04 -0500

    Use clojure 1.11.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 ;; NOTE: Used only for perf testing - this project is built with Maven (see pom.xml)
 (defproject clojure.data.json "1.1.1-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.11.0-rc1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :source-paths ["src/main/clojure"]
   :java-source-paths ["src/main/java"]
   :java-test-paths ["src/test/java"]
